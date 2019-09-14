142022b1724b59d33f18d6ed565bb02af78517cf
XiaoMing <888@163.com>
2019-09-15 06:45:03 +0800

    新增GET/POST接口

--- web/views.py
+++ web/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http.response import HttpResponse
+import json
+
 
 # Create your views here.
+def Login(request):
+    if request.method == 'POST':
+        result = {}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        result['username'] = username
+        result['password'] = password
+        result = json.dumps(result)
+        return HttpResponse(result, content_type="application/json;charset=utf-8")
+    else:
+        return render_to_response('login.html')
+
+
+'''
+def Login(request):
+    if request.method == 'GET':
+        result = {}
+        username = request.GET.get('username')
+        mobile = request.GET.get('mobile')
+        data = request.GET.get('data')
+        result['username'] = username
+        result['mobile'] = mobile
+        result['data'] = data
+        result = json.dumps(result)
+        return HttpResponse(result, content_type='application/json;charset=utf-8')
+    else:
+        return render_to_response('login.html')
+'''
