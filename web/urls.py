83c1c9c2ffab9d5c1eee994a0d70e19faed84f78
XiaoMing <888@163.com>
2020-09-25 03:01:56 +0800

    视图函数转类视图

--- web/urls.py
+++ web/urls.py
@@ -4,10 +4,16 @@ from . import views
 
 app_name = 'web'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     # 设计文章详情页的 URL
-    path('posts/<int:pk>/', views.detail, name='detail'),
-    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
-    path('categories/<int:pk>/', views.category, name='category'),
-    path('tags/<int:pk>/', views.tag, name='tag'),
+    # path('posts/<int:pk>/', views.detail, name='detail'),
+    # path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
+    # path('categories/<int:pk>/', views.category, name='category'),
+    # path('tags/<int:pk>/', views.tag, name='tag'),
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('archives/<int:year>/<int:month>/', views.ArchiveView.as_view(), name='archive'),
+    path('categories/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    path('tags/<int:pk>/', views.TagView.as_view(), name='tag'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render_to_response, get_object_or_404, render
 from django.http.response import HttpResponse
 # from django.http import HttpResponse
+
+from django.views.generic import ListView, DetailView
 # 引入 Post Category Tag 类
 from .models import Post, Category, Tag
 import json, markdown, re
 from markdown.extensions.toc import TocExtension
 from django.utils.text import slugify
 
-
+"""
 def index(request):
     # 首页配置
     # return HttpResponse("欢迎访问我的博客首页！")
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎访问我的博客首页',
+        # 'title': '我的博客首页',
+        # 'welcome': '欢迎访问我的博客首页',
         'post_list': post_list
     })
 
@@ -69,6 +71,89 @@ def tag(request, pk):
     t = get_object_or_404(Tag, pk=pk)
     post_list = Post.objects.filter(tags=t).order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
+"""
+
+
+# 将 index 视图函数改写为类视图
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+
+# 将 category 视图函数改写为类视图
+class CategoryView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        t = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=t)
+
+
+# 改写为类视图
+class ArchiveView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return (
+            super(ArchiveView, self)
+                .get_queryset()
+                .filter(created_time__year=year, created_time__month=month)
+        )
+
+
+# 记得在顶部导入 DetailView
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+
+        return post
 
 
 def Login(request):
